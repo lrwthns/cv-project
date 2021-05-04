a99e580e862877fb93990859707e1d23883817e9
lrwthns <lrwthns@gmail.com>
2021-05-04 22:47:29 +0700

    Create component files

---
+++ src/components/PersonalInfo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class PersonalInfo extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
